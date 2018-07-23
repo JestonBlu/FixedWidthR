50efbb346326612cf3c8f126e17fd81bdd96b1db
Jeston <JestonBlu@users.noreply.github.com>
2018-07-23 14:50:39 -0500

    Add files via upload

---
+++ R/checkFW.R
@@ -0,0 +1,9 @@
+#' Check the configurable object against the data frame input
+#'
+#' @param config Config list object
+#' @param data Data Frame you are intending to write
+fw.check <- function(config, data) {
+
+
+
+}
