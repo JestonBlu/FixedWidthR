d2887f885c19cf30040852e26e8bd251cfd174d7
Jeston <JestonBlu@users.noreply.github.com>
2018-07-24 14:02:23 -0500

    Delete checkFW.R

--- R/checkFW.R
+++
@@ -1,9 +0,0 @@
-#' Check the configurable object against the data frame input
-#'
-#' @param config Config list object
-#' @param data Data Frame you are intending to write
-fw.check <- function(config, data) {
-
-
-
-}
